666b675435857173acaf889401900a4c820032ef
Henner Zeller <h.zeller@acm.org>
2020-06-26 08:39:22 -0700

    SymbolFactory: Use idiomatic lookup/insert; fix bug.
     * Use the idiomatic way to lookup or insert into a
       map which can save a few cycles.
     * Fix bug: GetId() was returning zero-ID in case
       of a bad look-up, but zero is actually used by the
       first symbol. Changed that to 0xffffffff.
     * remove unnecessary includes in compilation unit.
    
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- templates/SymbolFactory.h
+++ templates/SymbolFactory.h
@@ -2,20 +2,20 @@
 
 /*
 
- Copyright 2019 Alain Dargelas
+  Copyright 2019 Alain Dargelas
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 /*
  * File:   SymbolFactory.h
@@ -34,40 +34,46 @@
 #include "uhdm_types.h"
 
 namespace UHDM {
-  class Serializer;
-  
-  class SymbolFactory {
-    friend Serializer;
-  public:
+class Serializer;
 
-    typedef unsigned int ID;
-    typedef std::vector<std::string> Id2SymbolMap;
-    typedef std::unordered_map<std::string, ID> Symbol2IdMap;
-
-    ID Make(const std::string& symbol);
-
-    const std::string& GetSymbol(ID id);
-
-    ID GetId(const std::string& symbol);
-
-  private:
-    ID idCounter_ = 0;
-    std::string bad_symbol_ = "@@BAD_SYMBOL@@";
-    Id2SymbolMap id2SymbolMap_;
-    Symbol2IdMap symbol2IdMap_;
-  };
-
-  class VectorOfanyFactory {
+class SymbolFactory {
   friend Serializer;
-  public:
-    std::vector<UHDM::any*>* Make() {
-      std::vector<UHDM::any*>* obj = new std::vector<UHDM::any*>();
+
+public:
+  typedef unsigned int ID;
+  typedef std::vector<std::string> Id2SymbolMap;
+  typedef std::unordered_map<std::string, ID> Symbol2IdMap;
+
+  // Register given "symbol" string as a symbol and return its id.
+  // If this is an existing symbol, its ID is returned, otherwise a new one
+  // is created.
+  ID Make(const std::string& symbol);
+
+  // Find id of given "symbol" or return "@@BAD_SYMBOL@@" if it doesn't exist.
+  const std::string& GetSymbol(ID id) const;
+
+  // Get symbol string identified by given ID or 0 (zero) if it doesn't exist
+  ID GetId(const std::string& symbol) const;
+
+private:
+  ID idCounter_ = 0;
+  Id2SymbolMap id2SymbolMap_;
+  Symbol2IdMap symbol2IdMap_;
+};
+
+class VectorOfanyFactory {
+  friend Serializer;
+
+public:
+  std::vector<UHDM::any*>* Make() {
+    std::vector<UHDM::any*>* obj = new std::vector<UHDM::any*>();
     objects_.push_back(obj);
     return obj;
   }
-  private:
-    std::vector<std::vector<UHDM::any*>*> objects_;
-  };
+
+private:
+  std::vector<std::vector<UHDM::any*>*> objects_;
+};
 
 };
 
