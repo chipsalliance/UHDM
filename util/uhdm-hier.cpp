93e55a5b6607bcdd1bbfe4367009bdc134df614d
Alain Dargelas <alain.dargelas@gmail.com>
2021-05-08 22:04:16 -1000

    New uhdm-hier Hierarchy dump utility

--- util/uhdm-hier.cpp
+++ util/uhdm-hier.cpp
@@ -28,10 +28,9 @@
 #include <sstream>
 #include <string>
 
-
 #if !(defined(_MSC_VER) || defined(__MINGW32__) || defined(__CYGWIN__))
-#  include <dirent.h>
-#  include <unistd.h>
+#include <dirent.h>
+#include <unistd.h>
 #endif
 
 #include "headers/uhdm.h"
@@ -39,7 +38,7 @@
 
 using namespace UHDM;
 
-static int usage(const char *progname) {
+static int usage(const char* progname) {
   fprintf(stderr, "Usage:\n%s [options] <uhdm-file>\n", progname);
   fprintf(stderr,
           "Reads UHDM binary representation and prints hierarchy tree.\n");
@@ -52,8 +51,10 @@ int main(int argc, char** argv) {
   // Simple option parsing that works on all platforms.
   for (int i = 1; i < argc; ++i) {
     const std::string arg = argv[i];
-    if (uhdmFile.empty()) uhdmFile = arg;
-    else return usage(argv[0]);
+    if (uhdmFile.empty())
+      uhdmFile = arg;
+    else
+      return usage(argv[0]);
   }
 
   if (uhdmFile.empty()) {
@@ -76,47 +77,48 @@ int main(int argc, char** argv) {
   std::string result;
   for (vpiHandle design : restoredDesigns) {
     if (vpi_get(vpiType, design) == vpiDesign) {
-      result += "Design name: " + std::string(vpi_get_str(vpiName, design)) + "\n";
+      result +=
+          "Design name: " + std::string(vpi_get_str(vpiName, design)) + "\n";
       result += "Instance tree:\n";
 
       vpiHandle instItr = vpi_iterate(UHDM::uhdmtopModules, design);
       while (vpiHandle obj_h = vpi_scan(instItr)) {
-	std::function<std::string(vpiHandle, std::string)> inst_visit =
-          [&inst_visit](vpiHandle obj_h, std::string path) {
-            std::string res;
-            std::string objectName;
-            if (const char* s = vpi_get_str(vpiName, obj_h)) {
-              objectName = s;
-            }
-	    if (objectName.size()) {
-  	      res += path + objectName + "\n";
-	      path +=  objectName + ".";
-	    }
-            // Recursive tree traversal
-            vpiHandle subItr = vpi_iterate(vpiModule, obj_h);
-            while (vpiHandle sub_h = vpi_scan(subItr)) {
-              res += inst_visit(sub_h, path);
- 	      vpi_release_handle(sub_h);
-            }
-	    vpi_release_handle(subItr);
-	    subItr = vpi_iterate(vpiGenScopeArray, obj_h);
-            while (vpiHandle sub_h = vpi_scan(subItr)) {
-              res += inst_visit(sub_h, path);
- 	      vpi_release_handle(sub_h);
-            }
-	    vpi_release_handle(subItr);
-	    subItr = vpi_iterate(vpiGenScope, obj_h);
-            while (vpiHandle sub_h = vpi_scan(subItr)) {
-              res += inst_visit(sub_h, path);
- 	      vpi_release_handle(sub_h);
-            }
-	    vpi_release_handle(subItr);
-            return res;
-          };
-	result += inst_visit(obj_h, "");
-	vpi_release_handle(obj_h);
+        std::function<std::string(vpiHandle, std::string)> inst_visit =
+            [&inst_visit](vpiHandle obj_h, std::string path) {
+              std::string res;
+              std::string objectName;
+              if (const char* s = vpi_get_str(vpiName, obj_h)) {
+                objectName = s;
+              }
+              if (objectName.size()) {
+                res += path + objectName + "\n";
+                path += objectName + ".";
+              }
+              // Recursive tree traversal
+              vpiHandle subItr = vpi_iterate(vpiModule, obj_h);
+              while (vpiHandle sub_h = vpi_scan(subItr)) {
+                res += inst_visit(sub_h, path);
+                vpi_release_handle(sub_h);
+              }
+              vpi_release_handle(subItr);
+              subItr = vpi_iterate(vpiGenScopeArray, obj_h);
+              while (vpiHandle sub_h = vpi_scan(subItr)) {
+                res += inst_visit(sub_h, path);
+                vpi_release_handle(sub_h);
+              }
+              vpi_release_handle(subItr);
+              subItr = vpi_iterate(vpiGenScope, obj_h);
+              while (vpiHandle sub_h = vpi_scan(subItr)) {
+                res += inst_visit(sub_h, path);
+                vpi_release_handle(sub_h);
+              }
+              vpi_release_handle(subItr);
+              return res;
+            };
+        result += inst_visit(obj_h, "");
+        vpi_release_handle(obj_h);
       }
-      vpi_release_handle(instItr); 
+      vpi_release_handle(instItr);
     }
   }
   std::cout << result;
