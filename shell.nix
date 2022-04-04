076567f5d505a9bf77dc98151ae71609cee5f102
Henner Zeller <h.zeller@acm.org>
2022-04-04 15:52:58 -0700

    Add shell.nix to provide all necessary build requirements.
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

---
+++ shell.nix
@@ -0,0 +1,22 @@
+# This is a nix-shell for use with the nix package manager.
+# If you have nix installed, you may simply run `nix-shell`
+# in this repo, and have all dependencies ready in the new shell.
+
+{pkgs ? import (builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-2021-11";
+  # Commit hash
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+}) {}}:
+
+
+pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      cmake
+      python3
+      python39Packages.orderedmultidict
+    ];
+}
