23cb64ac48910a6b699bdb3c365254f2cc4f0ca7
Henner Zeller <h.zeller@acm.org>
2023-01-13 16:52:30 -0800

    Add dependencies used for UHDM_USE_HOST_* flags.

--- shell.nix
+++ shell.nix
@@ -2,21 +2,25 @@
 # If you have nix installed, you may simply run `nix-shell`
 # in this repo, and have all dependencies ready in the new shell.
 
-{pkgs ? import (builtins.fetchTarball {
-  # Descriptive name to make the store path easier to identify
-  name = "nixos-2021-11";
-  # Commit hash
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz";
-  # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
-}) {}}:
-
-
+{ pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs;
     [
       cmake
       python3
       python39Packages.orderedmultidict
+
+      # for UHDM_USE_HOST_{CAPNP,GTEST}
+      capnproto
+      gtest
+
+      # Ease development
+      ccache
+      ninja
+      clang-tools
     ];
+  shellHook =
+  ''
+    export CMAKE_CXX_COMPILER_LAUNCHER=ccache
+  '';
 }
