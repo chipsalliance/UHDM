49a20583413a3a9802b3c5ff0abdb3e7cede75ae
Thomas <thomas.burg@gmail.com>
2023-03-31 22:09:52 +0200

    uncomment test

--- python/tests/test_module.py
+++ python/tests/test_module.py
@@ -3,22 +3,21 @@ import uhdm
 
 
 class  test_module(unittest.TestCase):
-    pass
 
-#    def test_module(self):
-#        result = []
-#
-#        s  = uhdm.Serializer()
-#
-#        data = uhdm.buildTestDesign(s)
-#        modit = uhdm.vpi_iterate(uhdm.uhdmallModules,data[0])
-#        while(True):
-#            vpiObj = uhdm.vpi_scan(modit)
-#            if vpiObj is None:
-#                break
-#            result.append(uhdm.vpi_get_str(uhdm.vpiName,vpiObj))
-#
-#        self.assertEqual(set(result),set(["module2","module1"]))
-#
+    def test_module(self):
+        result = []
+
+        s  = uhdm.Serializer()
+
+        data = uhdm.buildTestDesign(s)
+        modit = uhdm.vpi_iterate(uhdm.uhdmallModules,data[0])
+        while(True):
+            vpiObj = uhdm.vpi_scan(modit)
+            if vpiObj is None:
+                break
+            result.append(uhdm.vpi_get_str(uhdm.vpiName,vpiObj))
+
+        self.assertEqual(set(result),set(["module2","module1"]))
+
 if __name__ == '__main__':
     unittest.main()
