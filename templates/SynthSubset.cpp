a585512539f360e9828c2bbc2da8d18c6c43f081
Alain Dargelas <alain.dargelas@gmail.com>
2023-12-10 21:48:44 -0800

    typespec aliasing

--- templates/SynthSubset.cpp
+++ templates/SynthSubset.cpp
@@ -355,16 +355,20 @@ bool SynthSubset::reportedParent(const any* object) {
 }
 
 // Apply some rewrite rule for Synlig limitations, namely Synlig handles aliased typespec incorrectly.
-void SynthSubset::leaveRef_typespec(const ref_typespec* object, vpiHandle handle) {
+void SynthSubset::leaveRef_typespec(const ref_typespec* object,
+                                    vpiHandle handle) {
   if (const typespec* actual = object->Actual_typespec()) {
     if (const ref_typespec* ref_alias = actual->Typedef_alias()) {
-      // Make the typespec point to the aliased typespec if they are of the same type:
+      // Make the typespec point to the aliased typespec if they are of the same
+      // type:
       //   typedef lc_tx_e lc_tx_t;
       // When extra dimensions are added using a packed_array_typespec like in:
       //  typedef lc_tx_e [1:0] lc_tx_t;
       //  We will need to uniquify and create a new typespec instance
-      if (ref_alias->Actual_typespec()->UhdmType() == actual->UhdmType()) {
-        ((ref_typespec*)object)->Actual_typespec((typespec*) ref_alias->Actual_typespec());
+      if ((ref_alias->Actual_typespec()->UhdmType() == actual->UhdmType()) &&
+          !ref_alias->Actual_typespec()->VpiName().empty()) {
+        ((ref_typespec*)object)
+            ->Actual_typespec((typespec*)ref_alias->Actual_typespec());
       }
     }
   }
